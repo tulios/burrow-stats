57943c68075dc4ab7375286f82d441de5b147bfd
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-06-01 15:24:16 +0200

    configurable scaleFontSize

--- client/utils/chart-options.js
+++ client/utils/chart-options.js
@@ -3,6 +3,7 @@ import SettingsStore from '../utils/settings-store'
 const defaultSettings =  {
   animation: false,
   responsive: true,
+  scaleFontSize: 14,
   scaleFontColor: '#879db7',
   scaleLineColor: 'rgba(255, 255, 255, 0.3)',
   scaleGridLineColor : 'rgba(255, 255, 255, 0.06)',
