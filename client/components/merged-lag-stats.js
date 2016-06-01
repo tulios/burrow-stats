56914f7f361b1dcafdccd6384fc5d7512ee47af7
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-06-01 14:02:30 +0200

    Add support for plotting charts in different colors

--- client/components/merged-lag-stats.js
+++ client/components/merged-lag-stats.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Line as LineChart } from 'react-chartjs';
 import moment from 'moment'
 import cache from '../utils/cache'
-import chartEntry from '../utils/chart-entry'
+import chartEntry, {chartPalette} from '../utils/chart-entry'
 import chartOptions from '../utils/chart-options'
 
 import Widget from './widget'
@@ -27,7 +27,8 @@ export default React.createClass({
     return {
       labels: caches[0].series.map(entry => moment(entry.time).format('H:mm:ss')),
       datasets: caches.map((consumerCache, i) => {
-        return chartEntry(consumerCache.series.map(entry => entry.totalLag), consumerNames[i])
+        const palette = chartPalette(i)
+        return chartEntry(consumerCache.series.map(entry => entry.totalLag), consumerNames[i], palette)
       })
     }
   }
--- client/utils/chart-entry.js
+++ client/utils/chart-entry.js
@@ -1,12 +1,92 @@
+export function chartPalette(i) {
+  const palette = [
+    {
+      fillColor: 'rgba(0, 137, 207, 0.3)',
+      strokeColor: 'rgba(0, 137, 207, 1)',
+      pointColor: 'rgba(0, 137, 207, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(0, 137, 207, 1)',
+    },
+    {
+      fillColor: 'rgba(142, 142, 142, 0.3)',
+      strokeColor: 'rgba(142, 142, 142, 1)',
+      pointColor: 'rgba(142, 142, 142, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(142, 142, 142, 1)',
+    },
+    {
+      fillColor: 'rgba(250, 164, 58, 0.3)',
+      strokeColor: 'rgba(250, 164, 58, 1)',
+      pointColor: 'rgba(250, 164, 58, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(250, 164, 58, 1)',
+    },
+    {
+      fillColor: 'rgba(96, 189, 104, 0.3)',
+      strokeColor: 'rgba(96, 189, 104, 1)',
+      pointColor: 'rgba(96, 189, 104, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(96, 189, 104, 1)',
+    },
+    {
+      fillColor: 'rgba(241, 88, 84, 0.3)',
+      strokeColor: 'rgba(241, 88, 84, 1)',
+      pointColor: 'rgba(241, 88, 84, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(241, 88, 84, 1)',
+    },
+    {
+      fillColor: 'rgba(241, 124, 176, 0.3)',
+      strokeColor: 'rgba(241, 124, 176, 1)',
+      pointColor: 'rgba(241, 124, 176, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(241, 124, 176, 1)',
+    },
+    {
+      fillColor: 'rgba(222, 207, 63, 0.3)',
+      strokeColor: 'rgba(222, 207, 63, 1)',
+      pointColor: 'rgba(222, 207, 63, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(222, 207, 63, 1)',
+    },
+    {
+      fillColor: 'rgba(178, 118, 178, 0.3)',
+      strokeColor: 'rgba(178, 118, 178, 1)',
+      pointColor: 'rgba(178, 118, 178, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(178, 118, 178, 1)',
+    },
+    {
+      fillColor: 'rgba(178, 145, 47, 0.3)',
+      strokeColor: 'rgba(178, 145, 47, 1)',
+      pointColor: 'rgba(178, 145, 47, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(178, 145, 47, 1)',
+    },
+    {
+      fillColor: 'rgba(137, 137, 207, 0.3)',
+      strokeColor: 'rgba(137, 137, 207, 1)',
+      pointColor: 'rgba(137, 137, 207, 1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(137, 137, 207, 1)',
+    }
+  ]
+  return palette[i || 0] || palette[0]
+}
+
 export default function(data, label, merge) {
   return Object.assign({
     label: label,
-    fillColor: 'rgba(151, 187, 205, 0.3)',
-    strokeColor: 'rgba(0, 137, 207, 1)',
-    pointColor: 'rgba(0, 137, 207, 1)',
-    pointStrokeColor: '#fff',
-    pointHighlightFill: '#fff',
-    pointHighlightStroke: 'rgba(0, 137, 207, 1)',
     data: data
-  }, merge || {})
+  }, merge || chartPalette())
 }
